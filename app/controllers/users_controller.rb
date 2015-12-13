a492449269b2d99768fe7c8c619913c89a8a0310
yendou4 <in_japan_yuen@i.softbank.jp>
2015-12-13 08:43:46 +0000

    mod index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      #更新に成功したときの処理
+      flash[:success]="Profile updated"
+      redirect_to @user
     else
      render'edit'
     end
