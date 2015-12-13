ddfcedd27fd2590102c1413dda35d0e14bc0dfb5
yendou4 <in_japan_yuen@i.softbank.jp>
2015-12-13 05:09:07 +0000

    mod index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end 
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #更新に成功したときの処理
+    else
+     render'edit'
+    end
+  end
+    
   def create
     @user = User.new(user_params)
     if @user.save
