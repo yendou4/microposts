54e96e199e23afb7f532f1a72f4b6c9a52b9db6e
yendou4 <in_japan_yuen@i.softbank.jp>
2015-12-15 13:24:54 +0000

    mod users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,9 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      redirect_to root_url
+  　end
   end 
 
   def update
