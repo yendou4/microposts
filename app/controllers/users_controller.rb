dadf06f3d82138cb80bae34643e775773fbcb6b9
yendou4 <in_japan_yuen@i.softbank.jp>
2015-12-18 11:38:53 +0000

    challenge 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :profile, :area)
   end
   
   def set_user
